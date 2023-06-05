c0ae7fc64f70c870efb315122dea09000007e03c
dev-pengi <siffffdoul@gmail.com>
2023-06-05 16:25:14 +0100

    Update the creator

--- src/components/SvgCreator.tsx
+++ src/components/SvgCreator.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FC } from "react";
 import Controller from "./Controller";
 import { SvgBlob } from "react-svg-blob";
@@ -9,9 +9,10 @@ const SvgCreator: FC = () => {
   const [angels, setAngels] = useState(3);
   const [circle, setCircle] = useState(9);
 
-  useEffect(() => {
-    console.log(angels);
-  }, [angels]);
+  const [color1, setColor1] = useState("#1FFBE8");
+  const [color2, setColor2] = useState("#34FF80");
+  const svgRef = useRef(null);
+
   return (
     <section>
       <h1 className="capitalize text-center text-3xl font-black">
@@ -20,17 +21,22 @@ const SvgCreator: FC = () => {
       <div
         className={`${contentPadding} w-full max-w-[1100px] mx-auto mt-16 flex md:flex-row flex-col-reverse md:gap-1 gap-7 items-center justify-between`}
       >
-        <SvgBlob
-          shapeProps={{ growth: circle, edges: angels }}
-          variant="gradient"
-          colors={["#1FFBE8", "#34FF80"]}
-          className="w-[97%] max-w-[400px]"
-          width={400}
-          height={400}
-        />
+        <div className="w-[97%] max-w-[400px]">
+          <SvgBlob
+            ref={svgRef}
+            shapeProps={{ growth: circle, edges: angels }}
+            variant="gradient"
+            colors={[color1, color2]}
+          />
+        </div>
         <Controller
           handleAngleChange={(value) => setAngels(value as number)}
           handleCircleChange={(value) => setCircle(value as number)}
+          svgRef={svgRef}
+          color1={color1}
+          color2={color2}
+          setColor1={setColor1}
+          setColor2={setColor2}
         />
       </div>
     </section>
