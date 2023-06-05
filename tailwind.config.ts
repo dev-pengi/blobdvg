478924f3a4ddb13eb57d9dcc9ff7ae48255aedff
dev-pengi <siffffdoul@gmail.com>
2023-06-05 16:24:33 +0100

    Updated custom shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,11 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      boxShadow: {
+        light: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+        strong:
+          "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+      },
       screens: {
         vsm: "370px",
         xmd: "950px",
